b4ad8be30427256b1475e2de1f7c7d2bed2c9193
NnamdiAnyaele <nnamdianya2011@gmail.com>
2022-02-22 08:02:45 +0100

    completed search test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+/* eslint-disable testing-library/no-node-access */
+/* eslint-disable testing-library/no-unnecessary-act */
+import React from "react";
+import ReactTestUtils from "react-dom/test-utils";
+import { act, render } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import faker from "faker";
+import Search from "./pages/Search";
+import searchGif from "./services/giphyService";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock("./services/giphyService");
+
+describe("Search Component", () => {
+	test("for search input", () => {
+		act(() => {
+			render(
+				<Router>
+					<Search />
+				</Router>
+			);
+		});
+
+		//  Enter search term
+		act(() => {
+			const searchBox = document.querySelector("#search");
+			searchBox.value = "test";
+			ReactTestUtils.Simulate.change(searchBox);
+		});
+
+		const searchBox = document.querySelector("#search");
+		expect(searchBox.value).toBe("test");
+	});
 });
